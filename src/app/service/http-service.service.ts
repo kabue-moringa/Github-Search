3eacf5f35aa81cb6c0f657d443c4062b647ed31f
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-06-13 17:20:57 +0300

    make changes

--- src/app/service/http-service.service.ts
+++ src/app/service/http-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Users } from '../users';
-import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
--- src/app/service/repo-service.service.ts
+++ src/app/service/repo-service.service.ts
@@ -9,10 +9,9 @@ import { Observable } from 'rxjs';
 export class ReposServiceService {
   _URL = 'https://api.github.com/users/';
   token = '?access_token=' + environment.GithubKey;
-    constructor(public http: HttpClient ) { 
-  
+    constructor(public http: HttpClient ) {
     }
-  
+
     getRepoInfo(username: string): Observable<any> {
       return this.http.get(this._URL + username + "/repos?order=created&sort=asc?access_token=")
     }
